2b6266df389cb25143183723953771f7f5e5637b
Alfonsas <alfonsasalubeckas@gmail.com>
2026-01-04 18:50:07 +0200

    Task 05: Santa’s Magical ASCII Countdown

---
+++ tasks/task-05-ascii-countdown/task_5.js
@@ -0,0 +1,107 @@
+const DIGITS = [
+` █████ 
+ █   █ 
+ █   █ 
+ █   █ 
+ █████ `,
+
+`   █   
+  ██   
+   █   
+   █   
+  ███  `,
+
+` █████ 
+     █ 
+ █████ 
+ █     
+ █████ `,
+
+` █████ 
+     █ 
+ █████ 
+     █ 
+ █████ `,
+
+` █   █ 
+ █   █ 
+ █████ 
+     █ 
+     █ `,
+
+` █████ 
+ █     
+ █████ 
+     █ 
+ █████ `,
+
+` █████ 
+ █     
+ █████ 
+ █   █ 
+ █████ `,
+
+` █████ 
+     █ 
+     █ 
+     █ 
+     █ `,
+
+` █████ 
+ █   █ 
+ █████ 
+ █   █ 
+ █████ `,
+
+` █████ 
+ █   █ 
+ █████ 
+     █ 
+ █████ `
+];
+
+function renderNumber(num) {
+  if (num < 10) {
+    return DIGITS[num];
+}
+
+  const digits = String(num).split(""); // "10" → ["1", "0"]
+
+  const asciiLinesArray = digits.map(d => DIGITS[Number(d)].split("\n"));
+
+  const combinedLines = asciiLinesArray.reduce((acc, curr) =>
+    acc.map((line, i) => line + "   " + curr[i])
+  );
+
+  return combinedLines.join("\n");
+}
+
+
+let counter = 10;
+
+const messages = [
+  "Santa is preparing…",
+  "Reindeer are buckling up…",
+  "Sleigh warming up…"
+];
+
+const asciiElement = document.getElementById("ascii");
+const messageElement = document.getElementById("message");
+
+const interval = setInterval(() => {
+
+  asciiElement.textContent = renderNumber(counter);
+
+  messageElement.textContent = messages[counter % messages.length];
+
+  if (counter === 0) {
+    clearInterval(interval);
+
+    messageElement.textContent =
+      "🎅✨ SANTA’S SLEIGH IS LAUNCHING! ✨🎅";
+  }
+
+  counter--;
+}, 1000);
+
+
